44c888b03db07b7f839d2f244978160b14c6c557
stersylva <stersylva@gmail.com>
2018-08-27 09:36:56 -0300

    Ajuste na API

--- app/Task.php
+++ app/Task.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $table = "tasks";
-    protected $fillable = ['name'];
-        //, 'date_launch', 'status'];
+    protected $fillable = ['name'
+        , 'date_launch', 'status'];
 
     public static function doUpdate ($fields) {
         $task = Task::find($fields['id']);
